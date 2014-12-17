88cac9e93ff31b60df597d0d9ebaa4bb2b81a2ab
unknown <saon@697-334-Bard.sf.k12.internal>
2014-12-17 13:16:48 -0800

    updated google billboards

--- build-tmp/source/GoogleBillboard.java
+++ build-tmp/source/GoogleBillboard.java
@@ -22,10 +22,28 @@ public void setup()
 public void draw()  
 {   
 	//not needed for this assignment
+	for(int i=2;i<e.length()-10;i++)
+	{
+		String digits = e.substring(i,i+10); 
+        double dNum = Double.parseDouble(digits); 	
+
+        if(isPrime(dNum)==true)
+		{
+		System.out.println(digits);
+		break;
+		}
+	}
 }  
 public boolean isPrime(double dNum)  
 {   
     //to be finished later   
+    for(int i=2;i<=Math.sqrt(dNum);i++)
+  {
+   	if(dNum%i==0)
+  	{
+   	return false;
+  	}
+ }
     return true;  
 } 
   static public void main(String[] passedArgs) {
